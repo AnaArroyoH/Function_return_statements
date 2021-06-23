6f162426d9491ae24d01c8b344ecff4186eb8d0b
AnaArroyoH <75263257+AnaArroyoH@users.noreply.github.com>
2021-06-23 19:52:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//Checking if a number is big:
+const bigNumber = function (number) {
+    if (number > 100) {
+        return true;
+    } return false;
+}
+
+console.log(bigNumber(54));
+// console.log(bigNumber(101));
+
+//Bouncer at a club:
+const brendaBouncerBot = function ([age, peopleInside]) {
+    if (age < 18) {
+        return 'this is a club for adults';
+    } else if (peopleInside > 100) {
+        return "it's too busy now, come back later";
+    } else {
+        return 'come in';
+    }
+}
+
+// const enterClub = brendaBouncerBot([17, 98]);
+// const enterClub = brendaBouncerBot([23, 102]);
+const enterClub = brendaBouncerBot([34, 95]);
+console.log(enterClub);
+
+//Calculating the average:
+const calculateAverage = function (numbers) {
+    const average = numbers.reduce((total, n) => total + n) / numbers.lenght;
+    console.log(average);
+    // return average;
+};
+
+const randomNumbers = [3, 6, 4, 8, 2];
+calculateAverage(randomNumbers);
+
+const ages = [14, 56, 32, 44, 78];
+calculateAverage(ages);
